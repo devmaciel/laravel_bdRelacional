ea72b145c49f4c402064a4a39332436a19612b03
João Pedro Maciel <joomaciiel2@outlook.com>
2020-07-20 18:03:28 -0300

    aula_91

---
+++ database/migrations/2020_07_20_205315_create_database.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDatabase extends Migration
+{
+
+    public function up()
+    {
+        //======================================================
+        // Clientes
+        //======================================================
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->increments('id_cliente');
+            $table->string('nome');
+            $table->string('email');
+            $table->timestamps();
+        });
+
+        //======================================================
+        // Vendas
+        //======================================================
+        Schema::create('vendas', function (Blueprint $table) {
+            $table->increments('id_venda');
+            $table->unsignedInteger('id_cliente');
+            $table->string('produto');
+            $table->string('preco', 2);
+            $table->timestamps();
+
+            //Criação da Relação (chave estrangeira)
+            $table->foreign('id_cliente')
+                ->references('id_cliente')
+                ->on('clientes')
+                ->onDelete('cascade');
+        });
+    }
+
+    //---------------------------------------------------------------
+    public function down()
+    {
+        Schema::dropIfExists('teste');
+    }
+}
